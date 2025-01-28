d136ee70b1a7681dca74d165839f59bfd05071d6
RamaSandhy <rshandy07@gmail.com>
2025-01-28 21:05:05 +0700

    menambah fun

--- app/src/main/java/com/example/perkebunan/repository/CatatanRepository.kt
+++ app/src/main/java/com/example/perkebunan/repository/CatatanRepository.kt
@@ -1,5 +1,9 @@
 package com.example.perkebunan.repository
 
 class CatatanRepository {
-
+    fun getAllCatatanPanen(): List<CatatanPanen>
+    fun getCatatanPanenById(id: Int): CatatanPanen?
+    fun addCatatanPanen(catatanPanen: CatatanPanen)
+    fun updateCatatanPanen(catatanPanen: CatatanPanen)
+    fun deleteCatatanPanen(id: Int)
 }
