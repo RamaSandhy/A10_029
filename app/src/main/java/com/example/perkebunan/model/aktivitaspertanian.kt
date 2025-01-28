82d98560e9cb1ba44835086e74897ac313ca48ac
RamaSandhy <rshandy07@gmail.com>
2025-01-28 16:53:54 +0700

    membuat class aktivitaspertanian

--- app/src/main/java/com/example/perkebunan/model/aktivitaspertanian.kt
+++ app/src/main/java/com/example/perkebunan/model/aktivitaspertanian.kt
@@ -1,4 +1,34 @@
 package com.example.perkebunan.model
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
 class aktivitaspertanian {
+    @Entity(
+        tableName = "aktivitas_pertanian",
+        foreignKeys = [
+            ForeignKey(
+                entity = tanaman.Tanaman::class,
+                parentColumns = ["id_tanaman"],
+                childColumns = ["id_tanaman"],
+                onDelete = ForeignKey.CASCADE
+            ),
+            ForeignKey(
+                entity = pekerja.Pekerja::class,
+                parentColumns = ["id_pekerja"],
+                childColumns = ["id_pekerja"],
+                onDelete = ForeignKey.CASCADE
+            )
+        ]
+    )
+    data class AktivitasPertanian(
+        @PrimaryKey(autoGenerate = true)
+        val id_aktivitas: Int = 0,
+
+        val id_tanaman: Int,
+        val id_pekerja: Int,
+        val tanggal_aktivitas: String,
+        val deskripsi_aktivitas: String
+    )
 }
