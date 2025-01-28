ecfeba1f09d7841a048f74a729542363b6dd9f4e
RamaSandhy <rshandy07@gmail.com>
2025-01-28 20:55:04 +0700

    perbaikan awalnya fun menjadi abstract fun

--- app/src/main/java/com/example/perkebunan/repository/TanamanRepository.kt
+++ app/src/main/java/com/example/perkebunan/repository/TanamanRepository.kt
@@ -2,10 +2,10 @@ package com.example.perkebunan.repository
 
 import com.example.perkebunan.model.tanaman
 
-class TanamanRepository {
-    fun getAllTanaman(): List<tanaman.Tanaman>
-    fun getTanamanById(id: Int): tanaman.Tanaman?
-    fun addTanaman(tanaman: tanaman.Tanaman)
-    fun updateTanaman(tanaman: tanaman.Tanaman)
-    fun deleteTanaman(id: Int)
+abstract class TanamanRepository {
+    abstract fun getAllTanaman(): List<tanaman.Tanaman>
+    abstract fun getTanamanById(id: Int): tanaman.Tanaman?
+    abstract fun addTanaman(tanaman: tanaman.Tanaman)
+    abstract fun updateTanaman(tanaman: tanaman.Tanaman)
+    abstract fun deleteTanaman(id: Int)
 }
