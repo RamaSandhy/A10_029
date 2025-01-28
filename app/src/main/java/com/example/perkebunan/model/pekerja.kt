13f1310fe042d52cd0ab4588781807c268c4d68a
RamaSandhy <rshandy07@gmail.com>
2025-01-28 17:00:32 +0700

    menambahkan class pekerja

--- app/src/main/java/com/example/perkebunan/model/pekerja.kt
+++ app/src/main/java/com/example/perkebunan/model/pekerja.kt
@@ -1,4 +1,13 @@
 package com.example.perkebunan.model
 
+import androidx.room.PrimaryKey
+
 class pekerja {
+    data class Pekerja(
+        @PrimaryKey(autoGenerate = true)
+        val id_pekerja: Int = 0, // ID pekerja, auto-generate
+        val nama_pekerja: String, // Nama lengkap pekerja
+        val jabatan: String, // Jabatan pekerja
+        val kontak_pekerja: String // Informasi kontak pekerja
+    )
 }
