b13bf0eadf26cdaf55de683d5413d5972b3cf55f
RamaSandhy <rshandy07@gmail.com>
2025-01-28 16:59:53 +0700

    mengimport class tanaman di TanamanDao

--- app/src/main/java/com/example/perkebunan/dao/TanamanDao.kt
+++ app/src/main/java/com/example/perkebunan/dao/TanamanDao.kt
@@ -4,18 +4,19 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import com.example.perkebunan.model.tanaman
 import kotlinx.coroutines.flow.Flow
 
 interface TanamanDao {
     @Query("SELECT * FROM tanaman")
-    fun getAllTanaman(): Flow<List<Tanaman>>
+    fun getAllTanaman(): Flow<List<tanaman.Tanaman>>
 
     @Insert
-    suspend fun insertTanaman(tanaman: Tanaman)
+    suspend fun insertTanaman(tanaman: tanaman.Tanaman)
 
     @Update
-    suspend fun updateTanaman(tanaman: Tanaman)
+    suspend fun updateTanaman(tanaman: tanaman.Tanaman)
 
     @Delete
-    suspend fun deleteTanaman(tanaman: Tanaman)
+    suspend fun deleteTanaman(tanaman: tanaman.Tanaman)
 }
