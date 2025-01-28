4e533fd7db1ee821da5c93fd2c79aca4c4ec4df0
RamaSandhy <rshandy07@gmail.com>
2025-01-28 17:28:59 +0700

    menambahkan class pekerja ke PekerjaDao

--- app/src/main/java/com/example/perkebunan/dao/PekerjaDao.kt
+++ app/src/main/java/com/example/perkebunan/dao/PekerjaDao.kt
@@ -4,21 +4,22 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import com.example.perkebunan.model.pekerja
 import kotlinx.coroutines.flow.Flow
 
 interface PekerjaDao {
     @Query("SELECT * FROM pekerja")
-    fun getAllPekerja(): Flow<List<Pekerja>>
+    fun getAllPekerja(): Flow<List<pekerja.Pekerja>>
 
     @Query("SELECT * FROM pekerja WHERE id_pekerja = :id")
-    suspend fun getPekerjaById(id: Int): Pekerja?
+    suspend fun getPekerjaById(id: Int): pekerja.Pekerja?
 
     @Insert
-    suspend fun insertPekerja(pekerja: Pekerja)
+    suspend fun insertPekerja(pekerja: pekerja.Pekerja)
 
     @Update
-    suspend fun updatePekerja(pekerja: Pekerja)
+    suspend fun updatePekerja(pekerja: pekerja.Pekerja)
 
     @Delete
-    suspend fun deletePekerja(pekerja: Pekerja)
+    suspend fun deletePekerja(pekerja: pekerja.Pekerja)
 }
