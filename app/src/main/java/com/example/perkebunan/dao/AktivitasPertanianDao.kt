66feffbe231eb02052f4599f7f0f8d4f79cfda3c
RamaSandhy <rshandy07@gmail.com>
2025-01-28 16:55:17 +0700

    mengimport class aktivitaspertanian di AktivitasPertanianDao

--- app/src/main/java/com/example/perkebunan/dao/AktivitasPertanianDao.kt
+++ app/src/main/java/com/example/perkebunan/dao/AktivitasPertanianDao.kt
@@ -4,24 +4,25 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import com.example.perkebunan.model.aktivitaspertanian
 import kotlinx.coroutines.flow.Flow
 
 interface AktivitasPertanianDao {
     @Query("SELECT * FROM aktivitas_pertanian")
-    fun getAllAktivitas(): Flow<List<AktivitasPertanian>>
+    fun getAllAktivitas(): Flow<List<aktivitaspertanian.AktivitasPertanian>>
 
     @Query("SELECT * FROM aktivitas_pertanian WHERE id_aktivitas = :id")
-    suspend fun getAktivitasById(id: Int): AktivitasPertanian?
+    suspend fun getAktivitasById(id: Int): aktivitaspertanian.AktivitasPertanian?
 
     @Query("SELECT * FROM aktivitas_pertanian WHERE id_tanaman = :idTanaman")
-    fun getAktivitasByTanamanId(idTanaman: Int): Flow<List<AktivitasPertanian>>
+    fun getAktivitasByTanamanId(idTanaman: Int): Flow<List<aktivitaspertanian.AktivitasPertanian>>
 
     @Insert
-    suspend fun insertAktivitas(aktivitas: AktivitasPertanian)
+    suspend fun insertAktivitas(aktivitas: aktivitaspertanian.AktivitasPertanian)
 
     @Update
-    suspend fun updateAktivitas(aktivitas: AktivitasPertanian)
+    suspend fun updateAktivitas(aktivitas: aktivitaspertanian.AktivitasPertanian)
 
     @Delete
-    suspend fun deleteAktivitas(aktivitas: AktivitasPertanian)
+    suspend fun deleteAktivitas(aktivitas: aktivitaspertanian.AktivitasPertanian)
 }
