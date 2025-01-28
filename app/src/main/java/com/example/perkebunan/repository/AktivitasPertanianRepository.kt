b7cffa40f6a73effc8fd918c15961c5cb3b33d1c
RamaSandhy <rshandy07@gmail.com>
2025-01-28 21:08:56 +0700

    perbaikan dari aktivitas ke AktivitasPertanian

--- app/src/main/java/com/example/perkebunan/repository/AktivitasPertanianRepository.kt
+++ app/src/main/java/com/example/perkebunan/repository/AktivitasPertanianRepository.kt
@@ -1,9 +1,11 @@
 package com.example.perkebunan.repository
 
+import com.example.perkebunan.model.aktivitaspertanian
+
 abstract class AktivitasPertanianRepository {
-    abstract fun getAllAktivitas(): List<Aktivitas>
-    abstract fun getAktivitasById(id: Int): Aktivitas?
-    abstract fun addAktivitas(aktivitas: Aktivitas)
-    abstract fun updateAktivitas(aktivitas: Aktivitas)
+    abstract fun getAllAktivitas(): List<aktivitaspertanian.AktivitasPertanian>
+    abstract fun getAktivitasById(id: Int): aktivitaspertanian.AktivitasPertanian?
+    abstract fun addAktivitas(aktivitas: aktivitaspertanian.AktivitasPertanian)
+    abstract fun updateAktivitas(aktivitas: aktivitaspertanian.AktivitasPertanian)
     abstract fun deleteAktivitas(id: Int)
 }
