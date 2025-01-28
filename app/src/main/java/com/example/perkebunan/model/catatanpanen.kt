834733074ed92df6de7f62bdb0a170618cbcd87f
RamaSandhy <rshandy07@gmail.com>
2025-01-28 17:00:19 +0700

    menambahkan class tanaman

--- app/src/main/java/com/example/perkebunan/model/catatanpanen.kt
+++ app/src/main/java/com/example/perkebunan/model/catatanpanen.kt
@@ -1,4 +1,15 @@
 package com.example.perkebunan.model
 
+import androidx.room.PrimaryKey
+import java.util.Date
+
 class catatanpanen {
+    data class CatatanPanen(
+        @PrimaryKey(autoGenerate = true)
+        val id_panen: Int = 0, // ID unik untuk setiap catatan panen
+        val id_tanaman: Int, // ID tanaman yang dipanen
+        val tanggal_panen: Date, // Tanggal panen dilakukan
+        val jumlah_panen: Double, // Jumlah hasil panen (misal: kilogram atau buah)
+        val keterangan: String // Keterangan tambahan tentang kondisi panen atau masalah yang ditemui
+    )
 }
--- app/src/main/java/com/example/perkebunan/model/tanaman.kt
+++ app/src/main/java/com/example/perkebunan/model/tanaman.kt
@@ -1,4 +1,13 @@
 package com.example.perkebunan.model
 
+import androidx.room.PrimaryKey
+
 class tanaman {
+    data class Tanaman(
+        @PrimaryKey(autoGenerate = true)
+        val id_tanaman: Int = 0,
+        val nama_tanaman: String,
+        val periode_tanam: String,
+        val deskripsi_tanaman: String
+    )
 }
